e9ff8a441ba20d025d735e4b55eeedaf9ac04bbe
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2019-03-26 12:59:01 -0400

    Working simport

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use failure::Fail;
+use lazy_static::lazy_static;
 use log::*;
 use mdbook::book::{Book, Chapter};
 use mdbook::preprocess::{Preprocessor, PreprocessorContext};
 use mdbook::BookItem;
+use regex::Regex;
 use std::path::{Path, PathBuf};
 
 /// The pre-processor that powers the mdbook-superimport plugin
@@ -33,15 +35,21 @@ fn process_chapter(chapter: &mut Chapter) -> mdbook::errors::Result<()> {
 
     let simports = Simport::parse_chapter(chapter);
 
-    for simport in simports {
+    // Iterate backwards through the simports so that we start by replacing the imports
+    // that are lower in the file first.
+    //
+    // This ensures that as we replace simports we aren't throwing off the start and end
+    // indices of other simports.
+    for simport in simports.iter().rev() {
         let new_content = match simport.read_content_between_tags() {
             Ok(new_content) => new_content,
-            Err(err) => panic!("{:#?}", err) // FIXME: Return failure with `?`
+            Err(err) => panic!("{:#?}", err), // FIXME: Return failure with `?`
         };
 
-        // TODO: Replace the line within the content with the new_content
-        // content = new_content
+        content = content.replace(simport.full_simport_text, &new_content);
     }
+
+    chapter.content = content;
 
     Ok(())
 }
@@ -74,52 +82,67 @@ struct Simport<'a> {
     ///
     /// ../book.toml
     file: PathBuf,
+    /// The text of this simport in the host_chapter
+    ///
+    /// {{ #simport some-file.txt@some-tag }}
+    full_simport_text: &'a str,
     /// Tags after the characters after an `@` symbol. When importing from a file
     /// Superimport will pull all text before and after the `@tag`
     ///
     /// Some(super-section)
-    tag: Option<&'a str>,
-    /// The line in the file that this simport occurs on
-    ///
-    /// 1
-    line: usize,
+    tag: &'a str,
+    /// Where in the chapter's bytes does this simport start?
+    start: usize,
+    /// Where in the chapter's bytes does this simport end?
+    end: usize,
+}
+
+// Wrapping in lazy_static ensures that our regex is only compiled once
+lazy_static! {
+  /// The regex that finds simports such as -> `{{ #simport some-file.txt@some-tag }}`
+  static ref RE: Regex = Regex::new(
+  r"(?x)                        # (?x) means insignificant whitespace mode
+                                # allows us to put comments and space things out.
+
+    \\\{\{\#.*\}\}                # escaped import such as `\{{ #simport some-file.txt@some-tag }}`
+
+  |                               # OR
+
+                                  # Non escaped import -> `{{ #simport some-file.txt@some-tag }}`
+    \{\{\s*                         # opening braces and whitespace
+    \#simport                       # #simport
+    \s+                             # separating whitespace
+    (?P<file>[a-zA-Z0-9\s_.\-/\\]+) # some-file.txt
+    @                               # @ symbol that denotes the name of a tag
+    (?P<tag>[a-zA-Z0-9_.\-]+)       # some-tag (alphanumeric underscores and dashes)
+    \s*\}\}                         # whitespace and closing braces
+  "
+  ).unwrap();
 }
 
 impl<'a> Simport<'a> {
     fn parse_chapter<'c>(chapter: &'c Chapter) -> Vec<Simport<'c>> {
-        let simports = chapter
-            .content
-            .lines()
-            .enumerate()
-            .filter(|(idx, line)| line.contains("#simport"))
-            // [ "{{#simport ./fixture.css@cool-css }}" ] -> Simport { ... }
-            .map(|(idx, simport_line)| {
-                let mut after_simport = simport_line.split("#simport");
-                after_simport.next().unwrap();
-                // ./fixture.css@cool-css
-                let after_simport = after_simport.next().unwrap();
+        let mut simports = vec![];
 
-                // [./fixture.css, cool-css }}
-                let mut pieces = after_simport.split("@");
+        let matches = RE.captures_iter(chapter.content.as_str());
 
-                let file = pieces.next().unwrap().trim();
+        for capture_match in matches {
+            // {{#simport ./fixture.css@cool-css }}
+            let full_capture = capture_match.get(0).unwrap();
+            let file = capture_match["file"].into();
+            let tag = capture_match.get(2).unwrap();
 
-                // cool-css }}
-                let mut remaining = pieces.next().unwrap().split(" ");
-                // cool-css
-                let tag = match remaining.next() {
-                    Some(tag) => Some(tag.trim()),
-                    None => None,
-                };
+            let simport = Simport {
+                host_chapter: chapter,
+                file,
+                full_simport_text: &chapter.content[full_capture.start()..full_capture.end()],
+                tag: &chapter.content[tag.start()..tag.end()],
+                start: full_capture.start(),
+                end: full_capture.end(),
+            };
 
-                Simport {
-                    host_chapter: &chapter,
-                    file: file.into(),
-                    tag,
-                    line: idx + 1
-                }
-            })
-            .collect();
+            simports.push(simport);
+        }
 
         simports
     }
@@ -134,7 +157,7 @@ enum TagError {
 impl<'a> Simport<'a> {
     // TODO: Clean up - don't need 3 iterations through the file.. Do it in for loop.
     fn read_content_between_tags(&self) -> Result<String, TagError> {
-        let tag = self.tag.unwrap();
+        let tag = self.tag;
 
         let chapter_dir = self.host_chapter.path.parent().unwrap();
         let path = Path::join(Path::new(&chapter_dir), &self.file);
@@ -183,8 +206,10 @@ mod tests {
         let expected_simports = vec![Simport {
             host_chapter: &tag_import_chapter,
             file: "./fixture.css".into(),
-            tag: Some("cool-css"),
-            line: 4
+            full_simport_text: "{{#simport ./fixture.css@cool-css }}",
+            tag: "cool-css",
+            start: 20,
+            end: 56,
         }];
 
         assert_eq!(simports, expected_simports);
@@ -203,6 +228,23 @@ mod tests {
 }"#;
 
         assert_eq!(content_between_tags.unwrap(), expected_content);
+    }
+
+    #[test]
+    fn replace_chapter() {
+        let mut tag_import_chapter = make_tag_import_chapter();
+
+        process_chapter(&mut tag_import_chapter);
+
+        let expected_content = r#"# Tag Import
+
+```md
+.this-will-be-included {
+  display: block;
+}
+```
+"#;
+        assert_eq!(tag_import_chapter.content.as_str(), expected_content);
     }
 
     // Create a chapter to represent our tag-import test case in the /book
